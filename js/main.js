206e1cdc32ef359d43d0ce535366513256dc0d82
choiiis <haemin0128@naver.com>
2022-03-16 01:25:01 +0900

    Change select to choose option or type manually

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// 도메인 직접 입력 or domain option 선택
+const domainListEl = document.querySelector('#domain-list')
+const domainInputEl = document.querySelector('#domain-txt')
+domainListEl.addEventListener('change', (event) => {
+  // option에 있는 도메인 선택 시
+  if(event.target.value !== "type") {
+    // 선택한 도메인을 input에 입력하고 disabled
+    domainInputEl.value = event.target.value
+    domainInputEl.disabled = true
+  } else { // 직접 입력
+    // input 내용 초기화 & 입력 가능하도록 변경
+    domainInputEl.value = ""
+    domainInputEl.disabled = false
+  }
+})
+
 // '출생 연도' 셀렉트 박스 option 목록 동적 생성
 const birthYearEl = document.querySelector('#birth-year')
 // option 목록 생성 여부 확인
